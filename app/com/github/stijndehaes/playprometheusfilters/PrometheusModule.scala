1161a6e0db5e66fed2a89a817560c1276da9b2f5
Stijn De Haes <stijn.de.haes@gmail.com>
2017-08-13 12:56:49 +0200

    Reset registry on module load to make sure that we can use hot reload of play framework

--- app/com/github/stijndehaes/playprometheusfilters/PrometheusModule.scala
+++ app/com/github/stijndehaes/playprometheusfilters/PrometheusModule.scala
@@ -6,6 +6,7 @@ import io.prometheus.client.CollectorRegistry
 class PrometheusModule extends AbstractModule {
 
   override def configure(): Unit = {
+    CollectorRegistry.defaultRegistry.clear()
     bind(classOf[CollectorRegistry]).toInstance(CollectorRegistry.defaultRegistry)
   }
 }
--- app/com/github/stijndehaes/playprometheusfilters/controllers/PrometheusController.scala
+++ app/com/github/stijndehaes/playprometheusfilters/controllers/PrometheusController.scala
@@ -5,12 +5,16 @@ import com.github.stijndehaes.playprometheusfilters.utils.WriterAdapter
 import com.google.inject.Inject
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.common.TextFormat
+import org.slf4j.LoggerFactory
 import play.api.http.HttpEntity
 import play.api.mvc._
 
 class PrometheusController @Inject()(registry: CollectorRegistry) extends Controller {
 
+  private val logger = LoggerFactory.getLogger(classOf[PrometheusController])
+
   def getMetrics = Action {
+    logger.trace("Metrics call received")
     val samples = new StringBuilder()
     val writer = new WriterAdapter(samples)
     TextFormat.write004(writer, registry.metricFamilySamples())
