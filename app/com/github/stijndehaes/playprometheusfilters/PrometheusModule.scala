2683cac6ddf1f4c88413e9d67e6b02c9b7ae2e2e
Joost den Boer <jdenboer@dhl.com>
2018-09-05 14:24:00 +0200

    Updated prometheus client version Update Play and Scala dependencies Added setting for automatic loading of default hotspot collectors Updated Readme

--- app/com/github/stijndehaes/playprometheusfilters/PrometheusModule.scala
+++ app/com/github/stijndehaes/playprometheusfilters/PrometheusModule.scala
@@ -3,11 +3,20 @@ package com.github.stijndehaes.playprometheusfilters
 import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
 import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.hotspot.DefaultExports
 
 class PrometheusModule extends Module {
+  private val defaultExportsKey = "play-prometheus-filters.default-exports"
 
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
     CollectorRegistry.defaultRegistry.clear()
+
+    configuration.getOptional[Boolean](defaultExportsKey).foreach { enabled =>
+      if (enabled) {
+        DefaultExports.initialize()
+      }
+    }
+
     Seq(
       bind[CollectorRegistry].to(CollectorRegistry.defaultRegistry)
     )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 // The Play plugin
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.12")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.18")
 
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "2.3")
 
